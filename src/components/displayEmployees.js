5ee99bf8c9410a312a1ef258e4cbab4b1c9cd687
Dlozi Mthethwa <dlozi.mthethwa@gmail.com>
2023-06-07 14:21:29 +0200

    CRUD DONE

--- src/components/displayEmployees.js
+++ src/components/displayEmployees.js
@@ -7,6 +7,19 @@ export default function DisplayEmployees({whosOnDisplay,setWhosOnDisplay}){
 
     const [employees, setEmployees] = useState([]);
     const [isPopupOpen, setPopupOpen] = useState(false);
+    const [inputValues, setInputValues] = useState({
+        id:"",
+        name: "",
+        surname: "",
+        email: "",
+        bio: "",
+        pic: "",
+        birthday: "",
+        position: "",
+        phone: "",
+      });
+
+    const [employeeListReload, setEmployeeListReload] = useState(false);
 
     const changeDisplay = ()=>{
         setWhosOnDisplay(1);
@@ -28,7 +41,7 @@ export default function DisplayEmployees({whosOnDisplay,setWhosOnDisplay}){
     
     useEffect(() => {
       getData();
-    }, []);
+    }, [employeeListReload]);
     
     //=========================================
 
@@ -37,6 +50,7 @@ export default function DisplayEmployees({whosOnDisplay,setWhosOnDisplay}){
         axios.delete("http://localhost:4000/Employees/"+emp_num)
         .then(response => {
         console.log(response.data);
+        setEmployeeListReload(!employeeListReload);
         })
         .catch(error => {
         if (error.response) {
@@ -53,79 +67,36 @@ export default function DisplayEmployees({whosOnDisplay,setWhosOnDisplay}){
     }
 
     //=========================================
-
-    let updateClientId;
+    const handleChange = (event) => {
+        const { id, value } = event.target;
+        setInputValues((prevInputValues) => ({
+          ...prevInputValues,
+          [id]: value,
+        }));
+      }
 
     const add = () => {
-        const updatedInputValues = {
-          name: document.getElementById("name").value,
-          surname: document.getElementById("surname").value,
-          email: document.getElementById("email").value,
-          bio: document.getElementById("bio").value,
-          pic: document.getElementById("pic").value,
-          birthday: document.getElementById("birthday").value,
-          position: document.getElementById("position").value,
-          phone: document.getElementById("phone").value,
-        };
-
-        axios.patch("http://localhost:4000/Employees/"+updateClientId, updatedInputValues)
+        axios.patch("http://localhost:4000/Employees/"+inputValues.id, inputValues)
         .then(response => console.log(response.data))
         .catch(error => console.error(error));
+        setEmployeeListReload(!employeeListReload);
         setPopupOpen(false);
       };
     
 
-    const update = (emp_num)=>{
-        updateClientId=emp_num;
-            console.log(emp_num);
+    const update = (employee)=>{
         setPopupOpen(true);
-    }
-
-    if(isPopupOpen)
-    {
-        return (
-            <div>
-                
-                {
-                    <h4>
-                        <span style={{color:'gray'}}>Employees ID: {employees.id}</span> 
-                    </h4>
-                }
-              {isPopupOpen && (
-                <div className="popup">
-                  <div className="formStyles">
-                    <label htmlFor="name">Name</label>
-                    <input type="text" id="name" />
-                    <br />
-                    <label htmlFor="surname">Surname</label>
-                    <input type="text" id="surname" />
-                    <br />
-                    <label htmlFor="email">Email</label>
-                    <input type="email" id="email" />
-                    <br />
-                    <label htmlFor="bio">Bio</label>
-                    <input type="text" id="bio" />
-                    <br />
-                    <label htmlFor="pic">Upload photo</label>
-                    <input type="url" id="pic" />
-                    <br />
-                    <label htmlFor="birthday">Date of birth</label>
-                    <input type="date" id="birthday" />
-                    <br />
-                    <label htmlFor="position">Job title</label>
-                    <input type="text" id="position" />
-                    <br />
-                    <label htmlFor="phone">Phone</label>
-                    <input type="text" id="phone" />
-                    <br /><br />
-                    <button onClick={add} style={{ marginRight: '5vw' }} className="limeButton w3-btn w3-border w3-border-black w3-round-large">
-                      Add Employee
-                    </button>
-                  </div>
-                </div>
-              )}
-            </div>
-          ); 
+        setInputValues({
+            id:employee.id,
+            name: employee.name,
+            surname: employee.surname,
+            email: employee.email,
+            bio: employee.bio,
+            pic: employee.pic,
+            birthday: employee.birthday,
+            position: employee.position,
+            phone: employee.phone,
+          });
     }
 
     return (
@@ -137,7 +108,42 @@ export default function DisplayEmployees({whosOnDisplay,setWhosOnDisplay}){
                 <button onClick={changeDisplay} className="limeButton w3-btn w3-border w3-border-black w3-card-4 w3-round-large" style={{width:'20vw',marginTop:'50vh'}}>Add a new Employee</button>                
             </div>
             {isPopupOpen?
-                <div className="flexVertical" style={{width:'50vw',overflow:'scroll',padding:'10px'}}>
+                <div className="popup" style={{width:'50vw',padding:'10px'}}>
+                    <div style={{backgroundColor:'white',padding:'50px',justifyContent:'center',width:'50vw'}}>
+                    <label htmlFor="name">Name</label>
+                    <input type="text" id="id"  value={inputValues.id} readOnly/>
+                    <br />
+                    <label htmlFor="name">Name</label>
+                    <input type="text" id="name"  value={inputValues.name} onChange={handleChange} />
+                    <br />
+                    <label htmlFor="surname">Surname</label>
+                    <input type="text" id="surname"  value={inputValues.surname} onChange={handleChange} />
+                    <br />
+                    <label htmlFor="email">Email</label>
+                    <input type="email" id="email"  value={inputValues.email} onChange={handleChange} />
+                    <br />
+                    <label htmlFor="bio">Bio</label>
+                    <input type="text" id="bio"  value={inputValues.bio} onChange={handleChange} />
+                    <br />
+                    <label htmlFor="pic">Upload photo</label>
+                    <input type="url" id="pic"  value={inputValues.pic} onChange={handleChange} />
+                    <br />
+                    <label htmlFor="birthday">Date of birth</label>
+                    <input type="date" id="birthday"  value={inputValues.birthday} onChange={handleChange} />
+                    <br />
+                    <label htmlFor="position">Job title</label>
+                    <input type="text" id="position"  value={inputValues.position} onChange={handleChange} />
+                    <br />
+                    <label htmlFor="phone">Phone</label>
+                    <input type="text" id="phone"  value={inputValues.phone} onChange={handleChange} />
+                    <br /><br />
+                    <button onClick={add} style={{ marginRight: '5vw' }} className="limeButton w3-btn w3-border w3-border-black w3-round-large">
+                        Add Employee
+                    </button>
+                    </div>
+                </div>
+                :
+                <div id='employeeList' className="flexVertical" style={{width:'50vw',overflow:'scroll',padding:'10px'}}>
                     {employees.map((employees) => (
                     <div key={employees.id} className='profile w3-panel w3-white w3-round-large' style={{padding:'10px',alignItems:'center'}}>
                         <div><img src={employees.pic} alt={employees.name} className='pic'></img></div>
@@ -153,42 +159,11 @@ export default function DisplayEmployees({whosOnDisplay,setWhosOnDisplay}){
                         </div>
                         <div>
                             <button className='w3-btn w3-white w3-border w3-border-red w3-round-large w3-text-red' onClick={() => remove(employees.id)}>Remove</button><br/><br/>
-                            <button className='w3-btn w3-white w3-border w3-border-green w3-round-large w3-text-green'  onClick={() => update(employees.id)}>Update</button>
+                            <button className='w3-btn w3-white w3-border w3-border-green w3-round-large w3-text-green'  onClick={() => update(employees)}>Update</button>
                         </div>
                     </div>
                     ))}
-                </div>:
-                <div className="popup" style={{width:'50vw',overflow:'scroll',padding:'10px'}}>
-                <div className="formStyles">
-                  <label htmlFor="name">Name</label>
-                  <input type="text" id="name" />
-                  <br />
-                  <label htmlFor="surname">Surname</label>
-                  <input type="text" id="surname" />
-                  <br />
-                  <label htmlFor="email">Email</label>
-                  <input type="email" id="email" />
-                  <br />
-                  <label htmlFor="bio">Bio</label>
-                  <input type="text" id="bio" />
-                  <br />
-                  <label htmlFor="pic">Upload photo</label>
-                  <input type="url" id="pic" />
-                  <br />
-                  <label htmlFor="birthday">Date of birth</label>
-                  <input type="date" id="birthday" />
-                  <br />
-                  <label htmlFor="position">Job title</label>
-                  <input type="text" id="position" />
-                  <br />
-                  <label htmlFor="phone">Phone</label>
-                  <input type="text" id="phone" />
-                  <br /><br />
-                  <button onClick={add} style={{ marginRight: '5vw' }} className="limeButton w3-btn w3-border w3-border-black w3-round-large">
-                    Add Employee
-                  </button>
                 </div>
-              </div>
             }
       </div>
     );
